1aef3731810ee125c68c544bf1abcdd0eb638af9
Joao Uva <joaouva@gmail.com>
2021-06-22 18:02:10 +0100

    fix bug in Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,7 +3,6 @@ import Feedbacks from "../components/feedbacks/Feedbacks";
 import Funcionalidades from "../components/funcionalidades/Funcionalidades";
 import Premium from "../components/premium/Premium"
 import Footer from "../components/footer/Footer"
-
 import Header from "../components/header/Header";
 import Novidades from "../components/novidades/Novidades";
 import Parceiros from "../components/parceiros/Parceiros";
